9c365988fcf7035ff7352341c7227d818ece3968
caseymusila <musilacasey@gmail.com>
2021-06-14 15:26:34 +0300

    add styles to html page

--- script/script.js
+++ script/script.js
@@ -1,108 +1,121 @@
- //General Array Function
-function MakeArray(n) {
-   this.length = n;
-   for (var i = 1; i <=n; i++) {
-     this[i] = 0;
-   }
-}
+function getAkanName() {
+    let varYear = document.getElementById("varYear").value;
+    let varMonth = Number(document.getElementById("varMonth").value);
+    let varDate = Number(document.getElementById("varDate").value);
+    let genders = document.getElementsByName("gender");
 
-//male Initialize Days of Week Array
-days = new MakeArray(7);
-days[0] = "Kwame"
-days[1] = "Kwasi"
-days[2] = "Kwadwo"
-days[3] = "Kwabena"
-days[4] = "Kwaku"
-days[5] = "Yaw"
-days[6] = "Kofi"
-//female Initialize Days of Week Array
-day = new MakeArray(7);
-day[0] = "Ama"
-day[1] = "Akosua"
-day[2] = "Adwoa"
-day[3] = "Abenaa"
-day[4] = "Akua"
-day[5] = "Yaa"
-day[6] = "Afua"
+    // function to get gender
+    function getGender() {
+        for (let gender of genders) {
+            if (gender.checked) {
+                return gender.value;
+            }
+        }
+    }
 
-//Initialize Months Array
-months = new MakeArray(12);
-months[1] = "January"
-months[2] = "February"
-months[3] = "March"
-months[4] = "April"
-months[5] = "May"
-months[6] = "June"
-months[7] = "July"
-months[8] = "August"
-months[9] = "September"
-months[10] = "October"
-months[11] = "November"
-months[12] = "December"
+    let varGender = getGender();
+    console.log(varGender);
 
-//Day of Week Function
-function compute(form) {
-   var val1 = parseInt(form.day.value, 10)
-   if ((val1 < 0) || (val1 > 31)) {
-      alert("Day is out of range")
-   }
-   var val2 = parseInt(form.month.value, 10)
-   if ((val2 < 0) || (val2 > 12)) {
-      alert("Month is out of range")
-   }
-   var val2x = parseInt(form.month.value, 10)
-   var val3 = parseInt(form.year.value, 10)
-   if (val3 < 1900) {
-      alert("You're that old!")
-   }
-   if (val2 == 1) {
-      val2x = 13;
-      val3 = val3-1
-   }
-   if (val2 == 2) {
-      val2x = 14;
-      val3 = val3-1
-   }
-   var val4 = parseInt(((val2x+1)*3)/5, 10)
-   var val5 = parseInt(val3/4, 10)
-   var val6 = parseInt(val3/100, 10)
-   var val7 = parseInt(val3/400, 10)
-   var val8 = val1+(val2x*2)+val4+val3+val5-val6+val7+2
-   var val9 = parseInt(val8/7, 10)
-   var val0 = val8-(val9*7)
-   form.result1.value = months[val2]+" "+form.day.value +", "+form.year.value
-   form.result2.value = days[val0]
+    // validation function for the month to make sure user enters a month between 1 and 12. Input type nummber in html ignores min and max length properties
+    function valMonth() {
+        if (varMonth < 1 || varMonth > 12) {
+            return false;
+        } else {
+            return true;
+        }
+    }
 
-}
-function tomm(form) {
-   var val1 = parseInt(form.day.value, 10)
-   if ((val1 < 0) || (val1 > 31) || (val1 = null) || (val1 = '')) {
-      alert("Day is out of range")
-   }
-   var val2 = parseInt(form.month.value, 10)
-   if ((val2 < 0) || (val2 > 12) || (val1 = null) || (val1 = '')) {
-      alert("Month is out of range")
-   }
-   var val2x = parseInt(form.month.value, 10)
-   var val3 = parseInt(form.year.value, 10)
-   if (val3 < 1900) {
-      alert("You're too old!")
-   }
-   if (val2 == 1) {
-      val2x = 13;
-      val3 = val3-1
-   }
-   if (val2 == 2) {
-      val2x = 14;
-      val3 = val3-1
-   }
-   var val4 = parseInt(((val2x+1)*3)/5, 10)
-   var val5 = parseInt(val3/4, 10)
-   var val6 = parseInt(val3/100, 10)
-   var val7 = parseInt(val3/400, 10)
-   var val8 = val1+(val2x*2)+val4+val3+val5-val6+val7+2
-   var val9 = parseInt(val8/7, 10)
-   var val0 = val8-(val9*7)
-   form.result1.value = months[val2]+" "+form.day.value +", "+form.year.value
-   form.result2.value = day[val0]
+
+
+
+    //validation function for days of the month.
+    function valDay() {
+        //leap year
+        if (varMonth === 2 && Number(varYear % 4 === 0)) {
+            if (varDate >= 1 && varDate <= 29) {
+                return true;
+            } else {
+                return false
+            }
+        }
+        //non leap year
+        else if (varMonth === 2 && Number(varYear % 4 != 0)) {
+            if (varDate >= 1 && varDate <= 28) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+        //30 days
+        else if (varMonth === 4 || varMonth === 6 || varMonth === 9 || varMonth === 11) {
+            if (varDate >= 1 && varDate <= 30) {
+                return true
+            } else {
+                return false;
+            }
+        }
+
+        //validation of the 31 days
+        else if (varMonth === 1 || varMonth === 3 || varMonth === 5 || varMonth === 7 || varMonth === 8 || varMonth === 10 || varMonth === 12) {
+            if (varDate >= 1 && varDate <= 31) {
+                return true
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+
+
+    }
+
+    //validation variables
+    let monthValid = valMonth();
+    let dayValid = valDay();
+
+    //Formula to determine day of birth given all input variables
+    let dayNumber = Math.floor((((Number(varYear.slice(0, 2)) / 4) - 2 * Number(varYear.slice(0, 2)) - 1) +
+        ((5 * Number(varYear.slice(2, 4)) / 4)) + ((26 * (varMonth + 1) / 10)) + varDate) % 7);
+
+    //arrays for days of the week
+    let weekDays = [
+        "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+    ];
+    //arrays for akan names for the males
+    let akanMale = [
+        "Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"
+    ];
+    //arrays for akan names for the females
+    let akanFemale = [
+        "Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"
+    ];
+
+    //selecting items on the arrays
+    let index;
+    // code to minus one from the day of the week since the array index for the weekDays will be 6 and not 7(weekDays[0] to weekDays[6])
+    if (dayNumber == 0) {
+        index = 6;
+    } else {
+        index = dayNumber - 1;
+    }
+
+    console.log(index);
+
+    if (varGender == "male" && monthValid && dayValid) {
+        document.getElementById('result').textContent = "Born on a " + weekDays[index] + " , Your Akan name is " + akanMale[index];
+        document.getElementById('result').style.fontSize = "25px";
+        document.getElementById('result').style.fontFamily = "Segoe UI";
+
+        return false;
+
+    } else if (varGender == "female" && monthValid && dayValid) {
+        document.getElementById('result').textContent = "Born on a " + weekDays[index] + " , Your Akan name is " + akanFemale[index];
+        document.getElementById('result').style.fontSize = "25px";
+        document.getElementById('result').style.fontFamily = "Segoe UI";
+
+        return false;
+
+    } else {
+        alert("Date error. Make sure you entered a\r valid number of days for that month");
+    }
 }
